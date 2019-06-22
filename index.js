2e69c2c6e10768352bbe91436b2cc762bddf2488
Dave Hylands <dhylands@gmail.com>
2019-06-21 22:01:03 -0700

    Fixed up adapter to work

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /**
- * index.js - Loads the GPIO adapter.
+ * index.js - Loads the Temperature adapter.
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -8,18 +8,8 @@
 
 'use strict';
 
-const fs = require('fs');
+const TemperatureAdapter = require('./rtl-433-adapter');
 
-function maybeLoadRtl433Adapter(addonManager, manifest, errorCallback) {
-  // Verify that we have write permissions to /sys/class/gpio/export. Under
-  // regular linux, this file is owned by root, so the server would need to
-  // run as the root user. On the Raspberry Pi, being a member of the gpio
-  // group allows non-root access.
-  //
-  // This file won't exist on Mac/Windows.
-
-  const loadRtl433Adapter = require('./rtl-433-adapter');
-  return loadRtl433Adapter(addonManager, manifest, errorCallback);
-}
-
-module.exports = maybeLoadRtl433Adapter;
+module.exports = function(adapterManager, manifest) {
+  new TemperatureAdapter(adapterManager, manifest);
+};
